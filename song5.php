fa3ac35ee68c0f294f1be08ca1626f1cb33fce5a
MayoresCristineJoy <mayores.cristinejoy@mscmarinduque.edu.ph>
2024-03-11 01:44:43 +0800

    Our Fifth Song

---
+++ song5.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fifth Song</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<?php
+echo "<div class='container'>";
+echo "<h1>Yellow by Coldplay</h1>";
+echo "\n";
+echo "\n";
+
+echo "<pre>";
+echo"\n[Intro]";
+echo"\nB / Badd11 / B   x2";
+echo"\n";
+echo"\n[Verse 1]";
+echo"\nB                                                F#";
+echo"\nLook at the stars, look how they shine for you,";
+echo "\n                                 E"; 
+echo"\nand everything you do,";
+echo"\n                           B";
+echo"\nYeah, they were all yellow";
+echo"\n                                       F#";
+echo"\nI came along, I wrote a song for you";
+echo"\n                            E";
+echo"\nand all the things you do";
+echo"\n                          B";
+echo"\nAnd it was called Yellow";
+echo"\n                          F#";
+echo"\nOh, then I took my turn";
+echo"\n                                E";
+echo"\nAh, what a thing to have done";
+echo"\n                       B";
+echo"\nAnd it was all yellow.";
+echo"\n";
+echo"\nIntro to chorus";
+echo"\n    B               B7sus4  B";
+echo"\ne|-----------------------------------|";
+echo"\nB|-----------------------------------|";
+echo"\nG|--8-8-8-8-8-8-8-8-9-9-9-9-8-8-8-8--|";
+echo"\nD|--9-9-9-9-9-9-9-9-7-7-7-7-9-9-9-9--|";
+echo"\nA|--9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9--|";
+echo"\nE|--7-7-7-7-7-7-7-7-7-7-7-7-7-7-7-7--|";
+echo"\n";
+echo"\n[Chorus]"; 
+echo"\nE           G#m              F#";
+echo "\nYour skiiiiiin, oh yeah your skin and bones."; 
+echo"\nE           G#m            F#";
+echo"\nTurn innnnnnnto, something beautiful.";
+echo"\nE           G#m            F#        E";
+echo"\nYou knooooooow, you know I love you so...";
+echo"\n                       B";
+echo"\nYou know I love you so";
+echo"\n";
+echo"\n[Verse 2]";
+echo"\nB                                       F#";
+echo"\nSwam across, I jumped across for you,";
+echo"\n                           E";
+echo"\nOh, what a thing to do,";
+echo"\n                           B";
+echo"\nCause you were all yellow.";
+echo"\n                                       F#";
+echo"\nI drew a line, I drew a line for you,";
+echo"\n                           E";
+echo"\nOh, what a thing to do,";
+echo"\n                           B";
+echo"\nAnd it was all yellow.";
+echo"\n";
+echo"\nIntro to chorus";
+echo"\n    B               B7sus4  B";
+echo"\ne|-----------------------------------|";
+echo"\nB|-----------------------------------|";
+echo"\nG|--8-8-8-8-8-8-8-8-9-9-9-9-8-8-8-8--|";
+echo"\nD|--9-9-9-9-9-9-9-9-7-7-7-7-9-9-9-9--|";
+echo"\nA|--9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9--|";
+echo"\nE|--7-7-7-7-7-7-7-7-7-7-7-7-7-7-7-7--|";
+echo"\n";
+echo"\n[Chorus]";
+echo"\nE           G#m              F#";
+echo"\nYour skiiiiiin, oh yeah your skin and bones";
+echo"\nE           G#m           F#";
+echo"\nTurn innnnnnnto something beautiful";
+echo"\nE           G#m             F#           E";
+echo"\nYou knooooooow, for you I'd bleed myself dry";
+echo"\n                         B"; 
+echo"\nFor you I'd bleed myself dry";
+echo"\n[Outro] x2  (Guitar 2 play chords on top)";
+echo"\n   B                                                                 F#";
+echo"\ne|----------------------------------------------------------------------------------|
+";
+echo"\nB|----------------------------------------------------------------------------------|";
+echo"\nG|-11-11-11-11-11-11-11-11-9-9-9-9-9-9-9-9-15-15-15-15-15-15-15-15-13-13-13-13-13---|";
+echo"\nD|----------------------------------------------------------------------------------|";
+echo"\nA|----------------------------------------------------------------------------------|";
+echo"\nE|----------------------------------------------------------------------------------|";
+echo"\n";
+echo"\n                    E                                                  B";
+echo"\ne|----------------------------------------------------------------------------------|";
+echo"\nB|----------------------------------------------------------------------------------|";
+echo"\nG|-13-13-13-6-6-6-6-6-6-6-6-4-4-4-4-4-4-4-4-11-11-11-11-11-11-11-11-9-9-9-9-9-9-9-9-|";
+echo"\nD|----------------------------------------------------------------------------------|";
+echo"\nA|----------------------------------------------------------------------------------|";
+echo"\nE|----------------------------------------------------------------------------------|";
+echo"\n";
+echo"\nAnd then this one time";
+echo"\n   F#                                                                E";
+echo"\ne|----------------------------------------------------------------------------------|";
+echo"\nB|----------------------------------------------------------------------------------|";
+echo"\nG|-15-15-15-15-15-15-15-15-13-13-13-13-13-13-13-13-6-6-6-6-6-6-6-6-4-4-4-4-4-4-4-4--|";
+echo"\nD|----------------------------------------------------------------------------------|";
+echo"\nA|----------------------------------------------------------------------------------|";
+echo"\nE|----------------------------------------------------------------------------------|";
+echo"\n";
+echo"\n[Outro Lyrics]";
+echo"\nIt's true, look how they shine for you,";
+echo"\nLook how they shine for you,";
+echo"\nLook how they shine for,";
+echo"\n"; 
+echo"\nlook how they shine for you,";
+echo"\nLook how they shine for you,";
+echo"\nLook how they shine.";
+echo"\n";
+echo"\n[Ending]";
+echo"\nB";
+echo"\nlook at the stars";
+echo"\n                              F#m";
+echo"\nlook how the shine for you";
+echo"\n                                    E";
+echo"\nand all the things that you do...";
+
+echo "</div>";
+?>
+
+</body>
+</html>
